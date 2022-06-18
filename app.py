ec67cb2cbbb2c372054b809a1e4c8df9df45aba2
Vincent <vincent.huang1116@gmail.com>
2022-06-18 15:27:22 +0800

    connect to email system

--- app.py
+++ app.py
@@ -1,27 +1,38 @@
 from pymongo import MongoClient
 from flask import Flask, render_template, request
 from DB import Forms
+import requests
+from config import EMAIL_SYSTEM_URL
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/email', methods=["POST"])
 def email_post():
     forms = Forms()
-    forms.addData(name= request.form["name"], email= request.form["email"])
+    name = request.form["name"]
+    email = request.form["email"]
+    forms.addData(name=name, email=email)
+
+    payload = {"name": name, "email": email}
+    r = requests.post(EMAIL_SYSTEM_URL + "welcomeEmail", data=payload)
+    print("post to email system: ")
+    print(r.text)
     return render_template('email_got.html')
+
 
 @app.route('/email', methods=["GET"])
 def email():
     forms = Forms()
     formsData = forms.getAllData()
     print(forms)
-    return render_template('email.html', formsData = formsData)
-
+    return render_template('email.html', formsData=formsData)
 
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 MONGO_URL = "mongodb+srv://Vincent:Vin-19950305@cluster0.iftyq.mongodb.net/?retryWrites=true&w=majority"
-
+EMAIL_SYSTEM_URL = "https://vincent-email-system.herokuapp.com/"
